44b58a2fa9aa42351f1a62761978851f55c5b7bd
vigneshpy <vickypotter2516@gmail.com>
2019-05-17 02:47:40 -0400

    added dynamic url

--- home/views.py
+++ home/views.py
@@ -7,7 +7,13 @@ from admin_app.models import AddCategory
 
 def index(request):
 
-    cat=AddCategory.objects.all()
+    cat=AddCategory.objects.all()[:5]
     print(cat)
  
-    return render(request,'index.html',{'list':cat})
+    return render(request,'home.html',{'list':cat})
+
+
+def  category(request,cat):
+    cat1=AddCategory.objects.all()
+    print(cat)
+    return render(request,'category.html',{'categoryname':cat,'list':cat1})
